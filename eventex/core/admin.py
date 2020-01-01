7a6615f8c5e26babe630e6223b3784a31dd9d7a4
Daniel Gonçalves <daniel@base4.com.br>
2019-12-31 22:02:02 -0200

    Added Contact model

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from django.utils.html import format_html
 
 from .models import Speaker
+from .models import Contact
+
+
+class ContactInline(admin.TabularInline):
+    model = Contact
+    extra = 1
 
 
 class SpeakerModelAdmin(admin.ModelAdmin):
+    inlines = [ContactInline]
     prepopulated_fields = {'slug': ('name',)}
     list_display = ['name', 'photo_img', 'website_link']
 
